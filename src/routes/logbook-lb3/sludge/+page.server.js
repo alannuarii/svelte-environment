56c7d08c13adaba380fcd036db7f9b6f492e6ba0
alannuarii <alannuariii@gmail.com>
2023-02-13 16:46:16 +0800

    Update

--- src/routes/logbook-lb3/sludge/+page.server.js
+++ src/routes/logbook-lb3/sludge/+page.server.js
@@ -1,5 +1,5 @@
 export const actions = {
-	default: async ({ request }) => {
+	default: async ({ request, params }) => {
 		const data = await request.formData();
 
 		const formData = new FormData();
@@ -8,12 +8,21 @@ export const actions = {
 		formData.append('status', data.get('status'));
 		formData.append('jumlah', data.get('jumlah'));
 
-		console.log(formData);
-		const res = await fetch('http://pltdktm.anpy.my.id/lingkungan/lb3', {
+		const res = await fetch(`http://127.0.0.1:5000/lingkungan/${params}`, {
 			method: 'POST',
 			body: formData
 		});
 
 		return res.json();
 	}
+};
+
+export const load = async (params) => {
+	const param = params.url.pathname;
+	const lb3 = param.split('/').pop();
+
+	const res = await fetch(`http://127.0.0.1:5000/lingkungan/${lb3}`);
+	const data = res.json();
+
+	return data;
 };
--- src/routes/logbook-lb3/sludge/+page.svelte
+++ src/routes/logbook-lb3/sludge/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import LogbookLb3 from '../../components/LogbookLB3.svelte';
+
+	export let data;
+	console.log(data.data);
 </script>
 
 <LogbookLb3 lb3="sludge" />
