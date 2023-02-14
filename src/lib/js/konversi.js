4a75abf894f0d39792475af04a574723b6e50d6e
alannuarii <alannuariii@gmail.com>
2023-02-15 07:27:47 +0800

    Update

---
+++ src/lib/js/konversi.js
@@ -0,0 +1,8 @@
+export const dataKonversi = [
+    {
+        jenis: 'Satu buah aki standard (aki mobil)',
+        nilaiSatuan: 10,
+        satuan: 'kg',
+        result: function(){this.nilaiSatuan} 
+    }
+]
