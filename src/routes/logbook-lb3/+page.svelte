4e88315fe42c8f56bdf9e4be13e8fa2410df123b
alannuarii <alannuariii@gmail.com>
2023-02-14 17:59:31 +0800

    Update

--- src/routes/logbook-lb3/+page.svelte
+++ src/routes/logbook-lb3/+page.svelte
@@ -8,7 +8,7 @@
 			<h6>Sludge</h6>
 			<span class="kode">Kode Limbah <span class="badge text-bg-light">A332-1</span></span>
 			<a href="/logbook-lb3/sludge" class="position-absolute top-50 end-0 translate-middle-y me-4 "
-				><i class="bi-arrow-right-circle-fill text-primary" /></a
+				><i class="bi-arrow-right-circle text-primary" /></a
 			>
 		</div>
 		<div
@@ -17,7 +17,7 @@
 			<h6>Oli Bekas</h6>
 			<span class="kode">Kode Limbah <span class="badge text-bg-light">B105D</span></span>
 			<a href="/logbook-lb3/oli-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
-				><i class="bi-arrow-right-circle-fill text-primary" /></a
+				><i class="bi-arrow-right-circle text-primary" /></a
 			>
 		</div>
 		<div
@@ -26,7 +26,7 @@
 			<h6>Majun Bekas</h6>
 			<span class="kode">Kode Limbah <span class="badge text-bg-light">B110D</span></span>
 			<a href="/logbook-lb3/majun-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
-				><i class="bi-arrow-right-circle-fill text-primary" /></a
+				><i class="bi-arrow-right-circle text-primary" /></a
 			>
 		</div>
 		<div
@@ -35,7 +35,7 @@
 			<h6>Filter Bekas</h6>
 			<span class="kode">Kode Limbah <span class="badge text-bg-light">A303-3</span></span>
 			<a href="/logbook-lb3/filter-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
-				><i class="bi-arrow-right-circle-fill text-primary" /></a
+				><i class="bi-arrow-right-circle text-primary" /></a
 			>
 		</div>
 	</div>
