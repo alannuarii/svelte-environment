515a9a625a5529a3a5f815187bd9b9ee060c53f8
alannuarii <alannuariii@gmail.com>
2023-02-14 17:56:03 +0800

    Update

--- src/routes/logbook-lb3/+page.svelte
+++ src/routes/logbook-lb3/+page.svelte
@@ -1,31 +1,43 @@
 <script></script>
 
 <section class="text-center px-5">
-	<div class="list-group">
-		<a
-			href="/logbook-lb3/sludge"
-			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2"
-			><h6>Sludge</h6>
-			<span class="kode">Kode Limbah <span class="badge text-bg-light">A332-1</span></span></a
+	<div class="list-group text-start">
+		<div
+			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2 position-relative"
 		>
-		<a
-			href="/logbook-lb3/oli-bekas"
-			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2"
-			><h6>Oli Bekas</h6>
-			<span class="kode">Kode Limbah <span class="badge text-bg-light">B105D</span></span></a
+			<h6>Sludge</h6>
+			<span class="kode">Kode Limbah <span class="badge text-bg-light">A332-1</span></span>
+			<a href="/logbook-lb3/sludge" class="position-absolute top-50 end-0 translate-middle-y me-4 "
+				><i class="bi-arrow-right-circle-fill text-primary" /></a
+			>
+		</div>
+		<div
+			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2 position-relative"
 		>
-		<a
-			href="/logbook-lb3/majun-bekas"
-			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2"
-			><h6>Majun Bekas</h6>
-			<span class="kode">Kode Limbah <span class="badge text-bg-light">B110D</span></span></a
+			<h6>Oli Bekas</h6>
+			<span class="kode">Kode Limbah <span class="badge text-bg-light">B105D</span></span>
+			<a href="/logbook-lb3/oli-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
+				><i class="bi-arrow-right-circle-fill text-primary" /></a
+			>
+		</div>
+		<div
+			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2 position-relative"
 		>
-		<a
-			href="/logbook-lb3/filter-bekas"
-			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2"
-			><h6>Filter Bekas</h6>
-			<span class="kode">Kode Limbah <span class="badge text-bg-light">A303-3</span></span></a
+			<h6>Majun Bekas</h6>
+			<span class="kode">Kode Limbah <span class="badge text-bg-light">B110D</span></span>
+			<a href="/logbook-lb3/majun-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
+				><i class="bi-arrow-right-circle-fill text-primary" /></a
+			>
+		</div>
+		<div
+			class="list-group-item list-group-item-action bg-secondary text-light border border-0 mb-2 position-relative"
 		>
+			<h6>Filter Bekas</h6>
+			<span class="kode">Kode Limbah <span class="badge text-bg-light">A303-3</span></span>
+			<a href="/logbook-lb3/filter-bekas" class="position-absolute top-50 end-0 translate-middle-y me-4 "
+				><i class="bi-arrow-right-circle-fill text-primary" /></a
+			>
+		</div>
 	</div>
 </section>
 
@@ -39,5 +51,9 @@
 	}
 	.kode {
 		font-size: 15px;
+	}
+	i {
+		font-size: 40px;
+		cursor: pointer;
 	}
 </style>
