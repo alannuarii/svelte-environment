31fa130c5305f5f3679e24c07cb51cf2473fc418
alannuarii <alannuariii@gmail.com>
2023-02-14 15:13:57 +0800

    Update

--- src/routes/components/Menu.svelte
+++ src/routes/components/Menu.svelte
@@ -1,5 +1,9 @@
+<script>
+	let hideOffCanvas = 'hide';
+</script>
+
 <div
-	class="offcanvas offcanvas-end text-bg-dark"
+	class="offcanvas offcanvas-end text-bg-dark {hideOffCanvas}"
 	tabindex="-1"
 	id="offcanvasRight"
 	aria-labelledby="offcanvasRightLabel"
@@ -12,10 +16,15 @@
 		<div class="mb-3">
 			<p class="text-light-emphasis mb-2">Limbah B3</p>
 			<div class="list-group list-group-flush">
-				<a href="/saldo-lb3" class="list-group-item bg-secondary text-light border border-0 mb-2 rounded-2"
+				<a
+					href="/saldo-lb3"
+					class="list-group-item bg-secondary text-light border border-0 mb-2 rounded-2"
 					><i class="bi-trash2 me-2" />Saldo LB3</a
 				>
-				<a href="/logbook-lb3" class="list-group-item bg-secondary text-light border border-0 mb-2 rounded-2">
+				<a
+					href="/logbook-lb3"
+					class="list-group-item bg-secondary text-light border border-0 mb-2 rounded-2"
+				>
 					<i class="bi-journal-text me-2" />Logbook LB3
 				</a>
 				<a href="/" class="list-group-item bg-secondary text-light border border-0 mb-2 rounded-2">
