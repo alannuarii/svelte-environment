861714b0771aa0a86401ef0ec40597525caa2ff7
alannuarii <alannuariii@gmail.com>
2023-02-17 08:25:55 +0800

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
 	import Header from './components/Header.svelte';
+	import mobile from '../lib/img/mobile.svg';
 </script>
 
-<div id="master" class="bg-dark bg-opacity-100">
+<div id="master" class="bg-dark bg-opacity-100 d-sm-none">
 	<Header />
 	<main>
 		<slot />
 	</main>
 </div>
+<dir id="notmobile" class="d-none d-sm-block position-absolute top-50 start-50 translate-middle text-center">
+	<img src={mobile} class="img-fluid w-75 mb-5" alt="Open just for mobile" />
+	<h3 class="text-dark">Akses hanya untuk <span class="text-primary fw-bolder">mobile!</span></h3>
+</dir>
 
 <style>
 	#master {
