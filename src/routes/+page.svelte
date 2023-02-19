2428764911015de0d3a8307d2729eddbb54ff103
alannuarii <alannuariii@gmail.com>
2023-02-19 20:00:27 +0800

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <section>
-	<div class="lb3 bg-dark-subtle mx-2 px-2 py-3 mb-3 rounded-4 text-center">
+	<h1 class="text-center">DASHBOARD</h1>
+	<hr />
+	<div class="lb3 mx-2 px-2 py-3 mb-3 mt-4 text-center">
 		<div>
-			<h4 class="text-dark">Saldo LB3</h4>
+			<h4 class="text-primary">SALDO LB3</h4>
 		</div>
 		<div class="d-flex justify-content-evenly align-items-center">
 			<div class="card text-bg-secondary">
@@ -34,28 +36,51 @@
 			</div>
 		</div>
 	</div>
-	<div class="proses bg-dark-subtle mx-2 px-2 py-3 rounded-4 text-center">
+	<div class="proses mx-2 px-2 py-3 text-center mb-3">
 		<div>
-			<h4 class="text-dark">Total Debit Bulanan</h4>
+			<h4 class="text-primary">TOTAL DEBIT BULANAN</h4>
 		</div>
 		<div class="d-flex justify-content-evenly align-items-center">
 			<div class="card text-bg-secondary">
-				<div class="card-header d-flex align-items-center justify-content-center">Debit Limbah Proses</div>
+				<div class="card-header d-flex align-items-center justify-content-center">
+					Debit Limbah Proses
+				</div>
 				<div class="card-body">
 					<h5 class="card-title d-flex flex-column">0 <span>{satuan}</span></h5>
 				</div>
 			</div>
 			<div class="card text-bg-secondary">
-				<div class="card-header d-flex align-items-center justify-content-center">Debit Limbah Domestik</div>
+				<div class="card-header d-flex align-items-center justify-content-center">
+					Debit Limbah Domestik
+				</div>
 				<div class="card-body">
 					<h5 class="card-title d-flex flex-column">0 <span>{satuan}</span></h5>
 				</div>
 			</div>
 		</div>
 	</div>
+	<div class="d-flex justify-content-center">
+		<button
+			class="btn btn-primary"
+			data-bs-toggle="offcanvas"
+			data-bs-target="#offcanvasRight"
+			aria-controls="offcanvasRight">Menu</button
+		>
+	</div>
 </section>
 
 <style>
+	h1 {
+		font-weight: 900;
+	}
+	h4 {
+		font-weight: 700;
+	}
+	button {
+		height: 60px;
+		width: 150px;
+		border-radius: 40px 30px 50px 10px;
+	}
 	.lb3 .card {
 		width: 23%;
 	}
